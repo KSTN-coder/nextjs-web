5175c90e27c16fb2fc512a8c70825b406f4d8887
DESKTOP-DN91P43\tengn <tengneung.kittisit@hotmail.com>
2025-01-27 00:38:00 +0700

    Update src

--- my-app/src/app/layout.tsx
+++ my-app/src/app/layout.tsx
@@ -1,23 +1,14 @@
-import { Inter } from 'next/font/google';
+import { Inter, Montserrat } from 'next/font/google';
 import "./globals.css";
 
-
 const inter = Inter({ subsets: ['latin'] });
+const montserrat = Montserrat({ subsets: ['latin'], weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'] });
+
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en" className={inter.className}>
-      <head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap"
-          rel="stylesheet"
-        />
-      </head>
+    <html lang="en" className={`${inter.className} ${montserrat.className}`}>
       <body className="flex flex-col">
-
         <main className="flex-grow">{children}</main>
-
       </body>
     </html>
   );
